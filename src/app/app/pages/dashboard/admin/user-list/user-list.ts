869351b6b6df7b79fca1385a3f32eb367454774e
Juan D López <juandiegolopezortiz4@gmail.com>
2025-08-18 14:28:13 -0500

    User List Component

--- src/app/app/pages/dashboard/admin/user-list/user-list.ts
+++ src/app/app/pages/dashboard/admin/user-list/user-list.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../../../../core/services/user';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css'
 })
 export class UserList {
+  users: any[] = []
+  error = ''
 
+  constructor( private userSvc: UserService ) {}
+
+  ngOnInit() {
+    this.userSvc.listUsers().subscribe({
+      next: (u) => this.users = u,
+      error: () => this.error = 'No se pudieron cargar los usuarios'
+    })
+  }
 }
